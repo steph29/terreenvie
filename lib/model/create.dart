97088514aa2654b6a9e8d8863e1887ae31483f3e
Stephane Verardo <s.verardo29@gmail.com>
2023-04-05 22:34:38 +0200

    Add create hour function, update hours functiond, delete hours function. Add Seg;entation button for date choising

--- lib/model/create.dart
+++ lib/model/create.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:terreenvie/controller/AdminPage.dart';
+import 'package:terreenvie/controller/MainAppController.dart';
 
 import '../controller/DashboardPage.dart';
 
@@ -16,6 +18,9 @@ class Create extends StatefulWidget {
 class _CreateState extends State<Create> {
   TextEditingController posteController = TextEditingController();
   TextEditingController descController = TextEditingController();
+  TextEditingController debutController = TextEditingController();
+  TextEditingController finController = TextEditingController();
+  TextEditingController nbBenController = TextEditingController();
   User? userId = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
@@ -46,10 +51,43 @@ class _CreateState extends State<Create> {
             SizedBox(
               height: 30,
             ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 80),
+              child: TextFormField(
+                controller: debutController,
+                decoration: InputDecoration(hintText: "Début"),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 80),
+              child: TextFormField(
+                controller: finController,
+                decoration: InputDecoration(hintText: "fin"),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 80),
+              child: TextFormField(
+                controller: nbBenController,
+                decoration: InputDecoration(hintText: "Nombre de Bénévoles"),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
             ElevatedButton(
               onPressed: () async {
                 var poste = posteController.text.trim();
                 var desc = descController.text.trim();
+                var debut = debutController.text.trim();
+                var fin = finController.text.trim();
+                var nbBen = nbBenController.text.trim();
                 if (poste != "") {
                   try {
                     await FirebaseFirestore.instance
@@ -59,11 +97,18 @@ class _CreateState extends State<Create> {
                       "createdAt": DateTime.now(),
                       "poste": poste,
                       "desc": desc,
-                      "debut": "12h00",
-                      "fin": "16h00",
-                      "nbBen": 5,
+                      "hor": FieldValue.arrayUnion([
+                        {"debut": debut, "fin": fin, "nbBen": nbBen}
+                      ]),
                       "ben_id": userId?.uid,
                     });
+                    Navigator.pushReplacement(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (_, __, ___) => MainAppController(),
+                        transitionDuration: Duration(seconds: 0),
+                      ),
+                    );
                   } catch (e) {
                     print("Error $e");
                   }
