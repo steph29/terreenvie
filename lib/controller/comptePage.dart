bf99b41b605b8fbc4b377324ccc436899a35781e
Stephane Verardo <s.verardo29@gmail.com>
2023-06-07 22:36:05 +0200

    Manage to deploy checkbox by row. Need to select each one

--- lib/controller/comptePage.dart
+++ lib/controller/comptePage.dart
@@ -1,17 +1,326 @@
+import 'dart:js';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:scroll_date_picker/scroll_date_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:terreenvie/controller/AddPoste.dart';
+import 'package:terreenvie/controller/EditHoraire.dart';
+import 'package:terreenvie/controller/EditPoste.dart';
+import 'package:terreenvie/model/create.dart';
+import 'MainAppController.dart';
 
-class ComptePage extends StatelessWidget {
+class ComptePage extends StatefulWidget {
   const ComptePage({Key? key}) : super(key: key);
 
   @override
+  State<ComptePage> createState() => _ComptePageState();
+}
+
+class _ComptePageState extends State<ComptePage> {
+  TextEditingController posteContoller = TextEditingController();
+  TextEditingController descContoller = TextEditingController();
+  TextEditingController heureContoller = TextEditingController();
+  TextEditingController nbBenContoller = TextEditingController();
+
+  User? userId = FirebaseAuth.instance.currentUser;
+
+  String groupValue = "Samedi";
+
+  List<bool> checkedItems = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text(
-          'Compte Page',
-          style: TextStyle(fontSize: 30),
-        ),
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+              flex: 1,
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+                SizedBox(
+                  height: 30,
+                ),
+                buildSegmentControl(),
+                SizedBox(
+                  height: 30,
+                ),
+
+                SizedBox(
+                  height: 30,
+                ),
+                // Liste des cards
+                buildCard(groupValue),
+                SizedBox(
+                  height: 30,
+                ),
+              ])),
+        ],
       ),
-    );
+    ));
   }
+
+  Widget buildSegmentControl() => CupertinoSegmentedControl<String>(
+      padding: EdgeInsets.all(20),
+      groupValue: groupValue,
+      selectedColor: Color(0xFF2b5a72),
+      unselectedColor: Colors.white,
+      borderColor: Color(0xFF2b5a72),
+      pressedColor: Color(0xFF2b5a72).withOpacity(0.2),
+      children: {
+        "Mardi": buildSegment("Mardi"),
+        "Mercredi": buildSegment("Mercredi"),
+        "Jeudi": buildSegment("Jeudi"),
+        "Vendredi": buildSegment("Vendredi"),
+        "Samedi": buildSegment("Samedi"),
+        "Dimanche": buildSegment("Dimanche"),
+        "Lundi": buildSegment("Lundi"),
+      },
+      onValueChanged: (groupValue) {
+        print(groupValue);
+        setState(() {
+          this.groupValue = groupValue;
+        });
+      });
+
+  Widget buildCard(String groupValue) => StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("pos_hor")
+            .where("jour", isEqualTo: groupValue)
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something went wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CupertinoActivityIndicator());
+          }
+          if (snapshot.data!.docs.isEmpty) {
+            return Center(child: Text("No data found"));
+          }
+          if (snapshot != null && snapshot.data != null) {
+            return Center(
+              child: GridView.builder(
+                controller: ScrollController(),
+                shrinkWrap: true,
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (ctx, i) {
+                  var poste = snapshot.data!.docs[i]['poste'];
+                  var desc = snapshot.data!.docs[i]['desc'];
+                  var hor = snapshot.data?.docs[i]['hor'];
+                  var posteId = snapshot.data?.docs[i].id;
+                  return Card(
+                    color: Color(0xFFf2f0e7),
+                    child: Container(
+                      constraints:
+                          const BoxConstraints(minHeight: 0, maxHeight: 500.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20)),
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(5),
+                      child: Stack(
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    poste,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFF2b5a72)),
+                                  ),
+                                  // Bouton Ajouter un horaire
+                                  IconButton(
+                                    onPressed: () {
+                                      Get.to(() => AddPoste(), arguments: {
+                                        "poste": poste,
+                                        "desc": desc,
+                                        "posteId": posteId,
+                                      });
+                                    },
+                                    icon: Icon(Icons.edit),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Color(0xFF2b5a72),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                desc,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Color(0xFF2b5a72)),
+                              ),
+                              Column(
+                                children: [
+                                  // buildList(
+                                  // poste, hor, desc, posteId, snapshot, i),
+                                  buildCheckList(
+                                      poste, hor, desc, posteId, snapshot, i)
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 0.0,
+                  mainAxisSpacing: 5,
+                  mainAxisExtent: 500,
+                ),
+              ),
+            );
+          }
+          return Container();
+        },
+      );
+
+  Widget buildSegment(String text) => Container(
+        padding: EdgeInsets.all(12),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 20),
+        ),
+      );
+
+  Widget buildCheckList(poste, hors, desc, posteId, snapshot, i) =>
+      ListView.builder(
+        shrinkWrap: true,
+        itemCount: hors.length,
+        itemBuilder: (context, index) {
+          var horId = snapshot.data?.docs[i]['hor'][index];
+          var hord = snapshot.data?.docs[i]['hor'][index]["debut"];
+          var horf = snapshot.data?.docs[i]['hor'][index]["fin"];
+          var nben = snapshot.data?.docs[i]['hor'][index]['nbBen'];
+          return Card(
+            child: ListTile(
+              title: Text(
+                hord + ' - ' + horf + ' avec ' + nben + ' benevoles',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Color(0xFF2b5a72)),
+              ),
+              trailing: Container(
+                width: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Checkbox(
+                      value: checkedItems[index],
+                      onChanged: (value) {
+                        setState(() {
+                          checkedItems[index] = value!;
+                        });
+                      },
+                    )),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+
+  Widget buildList(poste, hors, desc, posteId, snapshot, i) => ListView.builder(
+      shrinkWrap: true,
+      itemCount: hors.length,
+      itemBuilder: (context, j) {
+        var horId = snapshot.data?.docs[i]['hor'][j];
+        var hord = snapshot.data?.docs[i]['hor'][j]["debut"];
+        var horf = snapshot.data?.docs[i]['hor'][j]["fin"];
+        var nben = snapshot.data?.docs[i]['hor'][j]['nbBen'];
+
+        // final sortedItems = hors
+        //   ..sort((item1, item2) => item2.compareTo(item1));
+        // final hor = sortedItems[j];
+
+        return Card(
+          child: ListTile(
+            title: Text(
+              hord + ' - ' + horf + ' avec ' + nben + ' benevoles',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Color(0xFF2b5a72)),
+            ),
+            trailing: Container(
+              width: 70,
+              child: Row(children: [
+                // Bouton DELETE
+                Expanded(
+                  child: IconButton(
+                    onPressed: () {
+                      print(groupValue);
+                    },
+                    icon: Icon(Icons.delete),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF2b5a72),
+                    ),
+                  ),
+                ),
+                // Bouton MODIFIER
+                Expanded(
+                  child: IconButton(
+                    onPressed: () {
+                      Get.to(() => EditPoste(), arguments: {
+                        "jour": groupValue,
+                        "poste": poste,
+                        "desc": desc,
+                        "horId": horId,
+                        "debut": hord,
+                        "fin": horf,
+                        "nbBen": nben,
+                        "posteId": posteId,
+                      });
+                    },
+                    icon: Icon(Icons.edit),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF2b5a72),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ),
+        );
+      });
 }
