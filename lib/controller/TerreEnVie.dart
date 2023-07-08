6ef4d3c21bc8848610334abde97876ea4d3599f7
Stephane Verardo <s.verardo29@gmail.com>
2023-07-08 13:53:08 +0200

    delete function and adding volunteers on pos_hor

--- lib/controller/TerreEnVie.dart
+++ lib/controller/TerreEnVie.dart
@@ -6,7 +6,7 @@ import 'package:url_launcher/url_launcher.dart';
 const String url = 'https://www.terreenvie.com/';
 
 class TerreEnVie extends StatelessWidget {
-  const TerreEnVie({super.key});
+  const TerreEnVie({key});
 
   @override
   Widget build(BuildContext context) {
--- lib/model/MyUser.dart
+++ lib/model/MyUser.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_database/firebase_database.dart';
 
 class MyUser {
-  String? uid;
-  String? prenom;
-  String? nom;
+  String uid;
+  String prenom;
+  String nom;
 
-  MyUser(DataSnapshot snapshot, {required String uid}) {
-    uid = snapshot.key!;
-    Object map = snapshot.value!;
-    prenom = (snapshot.value! as Map)['prenom'];
-    nom = (snapshot.value! as Map)['nom'];
+  MyUser(DataSnapshot snapshot, {String uid}) {
+    uid = snapshot.key;
+    Object map = snapshot.value;
+    prenom = (snapshot.value as Map)['prenom'];
+    nom = (snapshot.value as Map)['nom'];
   }
   Map toMap() {
     return {"prenom": prenom, "nom": nom, "uid": uid};
